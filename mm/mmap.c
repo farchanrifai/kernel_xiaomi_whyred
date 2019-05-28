52e367d2ccfe0eaeb9a42d32bdbcd26d6612006d
Sebastian Frias <sf84@laposte.net>
2019-05-27 23:03:33 -0300

    mm: add config option to select the initial overcommit mode
    This config option allows to setup the initial overcommit mode to any of
    the 3 available values, OVERCOMMIT_GUESS (which remains as default),
    OVERCOMMIT_ALWAYS and OVERCOMMIT_NEVER.
    The overcommit mode can still be changed thru sysctl after the system
    boots up.
    
    This config option depends on CONFIG_EXPERT.
    This patch does not introduces functional changes.
    
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -124,8 +124,13 @@ void vma_set_page_prot(struct vm_area_struct *vma)
 	}
 }
 
-
+#if defined(CONFIG_OVERCOMMIT_NEVER)
+int sysctl_overcommit_memory __read_mostly = OVERCOMMIT_NEVER;
+#elif defined(CONFIG_OVERCOMMIT_ALWAYS)
+int sysctl_overcommit_memory __read_mostly = OVERCOMMIT_ALWAYS;
+#else
 int sysctl_overcommit_memory __read_mostly = OVERCOMMIT_GUESS;  /* heuristic overcommit */
+#endif
 int sysctl_overcommit_ratio __read_mostly = 50;	/* default is 50% */
 unsigned long sysctl_overcommit_kbytes __read_mostly;
 int sysctl_max_map_count __read_mostly = DEFAULT_MAX_MAP_COUNT;
