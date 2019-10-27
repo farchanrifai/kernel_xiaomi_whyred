d3ea12f728960a81cec110c1e68e1aac7e01bf8c
srfarias <erikofarias@gmail.com>
2019-10-27 03:25:24 +0000

    Revert "ion/iommu: msm: force pushhh updates"
    This reverts commit ec1bfbc3a3a81317513ec3ec83a8b05d157ae46a.
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- include/linux/msm_dma_iommu_mapping.h
+++ include/linux/msm_dma_iommu_mapping.h
@@ -18,8 +18,9 @@
 #include <linux/scatterlist.h>
 #include <linux/dma-mapping.h>
 
+struct msm_iommu_meta;
 struct msm_iommu_data {
-	struct list_head map_list;
+	struct msm_iommu_meta *meta;
 	struct mutex lock;
 };
 
@@ -30,32 +31,32 @@ struct msm_iommu_data {
 * and unmapping.
 */
 int msm_dma_map_sg_attrs(struct device *dev, struct scatterlist *sg, int nents,
-		   enum dma_data_direction dir, struct dma_buf *dmabuf,
+		   enum dma_data_direction dir, struct dma_buf *dma_buf,
 		   struct dma_attrs *attrs);
 
 static inline int msm_dma_map_sg_lazy(struct device *dev,
 			       struct scatterlist *sg, int nents,
 			       enum dma_data_direction dir,
-			       struct dma_buf *dmabuf)
+			       struct dma_buf *dma_buf)
 {
-	return msm_dma_map_sg_attrs(dev, sg, nents, dir, dmabuf, NULL);
+	return msm_dma_map_sg_attrs(dev, sg, nents, dir, dma_buf, NULL);
 }
 
 static inline int msm_dma_map_sg(struct device *dev, struct scatterlist *sg,
 				  int nents, enum dma_data_direction dir,
-				  struct dma_buf *dmabuf)
+				  struct dma_buf *dma_buf)
 {
 	DEFINE_DMA_ATTRS(attrs);
 
 	init_dma_attrs(&attrs);
 	dma_set_attr(DMA_ATTR_NO_DELAYED_UNMAP, &attrs);
-	return msm_dma_map_sg_attrs(dev, sg, nents, dir, dmabuf, &attrs);
+	return msm_dma_map_sg_attrs(dev, sg, nents, dir, dma_buf, &attrs);
 }
 
-void msm_dma_unmap_sg(struct device *dev, struct scatterlist *sg, int nents,
-		      enum dma_data_direction dir, struct dma_buf *dmabuf);
+void msm_dma_unmap_sg(struct device *dev, struct scatterlist *sgl, int nents,
+		      enum dma_data_direction dir, struct dma_buf *dma_buf);
 
-void msm_dma_unmap_all_for_dev(struct device *dev);
+int msm_dma_unmap_all_for_dev(struct device *dev);
 
 /*
  * Below is private function only to be called by framework (ION) and not by
